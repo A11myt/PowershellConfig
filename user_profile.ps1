79a3caf745894b563a28529b089b9e9096174d8d
JasonEJohnson <johnson@take-e-way.de>
2024-04-16 08:54:35 +0200

    fixed default nvim path

--- user_profile.ps1
+++ user_profile.ps1
@@ -9,7 +9,7 @@ Set-Alias touch new-item
 Set-Alias open explorer
 Set-Alias mvdir Move-Item
 
-function WSN(){ Set-Location "%HOMEPATH%\AppData\Local\nvim"}
+function WSN(){ Set-Location ~\AppData\Local\nvim}
 function WS(){ Set-Location 'C:\Workspace\'}
 
 function WSPS(){ Set-Location 'C:\Workspace\Powershell'}
