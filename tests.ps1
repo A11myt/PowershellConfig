0f907f26ea3ffa8b90a0e49111fbf560cab1d11c
Jason <Jason.j@gmx.de>
2024-04-16 07:48:02 +0200

    cleanup

--- tests.ps1
+++
@@ -1,1 +0,0 @@
-move-item -path C:\Users\JohnsonJason\PowerShellTests\A\A.txt -destination C:\Users\JohnsonJason\PowerShellTests\B\A.txt
--- user_profile.ps1
+++ user_profile.ps1
@@ -3,19 +3,14 @@
 #Alias
 Set-Alias vim nvim 
 Set-Alias ll ls 
-Set-Alias clr clear
 Set-Alias g git
 Set-Alias grep findstr
 Set-Alias touch new-item
 Set-Alias open explorer
 Set-Alias mvdir Move-Item
 
-function WSN(){ Set-Location 'C:\Users\dev-adm\AppData\Local\nvim'}
+function WSN(){ Set-Location "%HOMEPATH%\AppData\Local\nvim"}
 function WS(){ Set-Location 'C:\Workspace\'}
 
 function WSPS(){ Set-Location 'C:\Workspace\Powershell'}
 
-#Schedule Task Executables
-Set-Alias lst 'C:\Workspace\Powershell\LST.ps1'
-Set-Alias cst 'C:\Workspace\Powershell\CST.ps1'
-Set-Alias rst 'C:\Workspace\Powershell\RST.ps1'
