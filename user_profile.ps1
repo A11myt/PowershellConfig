128ca4ff92534415602a0bb15a79fe19bc6fdbaf
JasonEJohnson <johnson@take-e-way.de>
2024-04-29 08:16:57 +0200

    changed the WS function to add folders behind if wanted

--- user_profile.ps1
+++ user_profile.ps1
@@ -10,7 +10,13 @@ Set-Alias open explorer
 Set-Alias mvdir Move-Item
 
 function WSN(){ Set-Location ~\AppData\Local\nvim}
-function WS(){ Set-Location 'C:\Workspace\'}
 
 function WSPS(){ Set-Location 'C:\Workspace\Powershell'}
 
+function WS($folder){
+    if($folder){
+        Set-Location "C:\Workspace\$folder"
+    } else {
+        Set-Location 'C:\Workspace'
+    }
+}
